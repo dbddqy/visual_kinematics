c0e70472a72efa6aa326f740b718083d2b3d889a
Yue <dbddqy@gmail.com>
2020-07-08 00:29:07 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# Visual-Kinematics
+
+This is a super simple but useful python package which helps to calculate the robot robot kinematics and visualizing trajectory in just a few lines of code.  
+
+You have to deal with neither vector and matrix algebra, nor inverse kinematics, as long as there are robot's D-H parameters, you are good to go.
+
+If you are unfamiliar with D-H parameters, please read [here](https://en.wikipedia.org/wiki/Denavit%E2%80%93Hartenberg_parameters).  
+
+# Usage
+
+4 examples files are given.
+
+...  
+(to be finished)
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="visual_kinematics",
+    version="0.0.1",
+    author="Yue QI",
+    author_email="dbddqy@gmail.com",
+    description="A package for calculating robot kinematics and visualizing trajectory in just a few lines of code",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dbddqy/visual_kinematics",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
