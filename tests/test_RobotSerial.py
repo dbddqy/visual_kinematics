7fa414f4c64255de2fb5fa1230e5e992a8c026c2
Tobias Spens <tobias.spens@wandelbots.com>
2022-07-21 12:22:04 +0200

    test for plotting several positions with slider

--- tests/test_RobotSerial.py
+++ tests/test_RobotSerial.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib.widgets import Slider
 
 from visual_kinematics.RobotSerial import RobotSerial
 from visual_kinematics.Tool import Tool
@@ -28,40 +29,72 @@ class TestRobotSerial(TestCase):
         [0, 0, 0, 1]
     ])
 
-    robot_with_tool = RobotSerial(
-        dh_params=dh_params,
-        dh_type="normal",
-        tool=Tool(t_4_4),
-        ws_lim=None
-    )
-    robot_no_tool = RobotSerial(
-        dh_params=dh_params,
-        dh_type="normal",
-        ws_lim=None
-    )
-    robot_specify_plot_size = RobotSerial(
-        dh_params=dh_params,
-        dh_type="normal",
-        plot_xlim=[-200, 150],
-        plot_ylim=[-500, 500],
-        plot_zlim=[0, 500],
-        ws_lim=None
-    )
-
     def test_draw(self):
+        robot_with_tool = RobotSerial(
+            dh_params=self.dh_params,
+            dh_type="normal",
+            tool=Tool(self.t_4_4),
+            ws_lim=None
+        )
+        robot_no_tool = RobotSerial(
+            dh_params=self.dh_params,
+            dh_type="normal",
+            ws_lim=None
+        )
+        robot_specify_plot_size = RobotSerial(
+            dh_params=self.dh_params,
+            dh_type="normal",
+            plot_xlim=[-200, 150],
+            plot_ylim=[-500, 500],
+            plot_zlim=[0, 500],
+            ws_lim=None
+        )
         joint_angles = np.array([1, -2, 2, 1, 1, 1])
 
         with self.subTest('without tool'):
-            self.robot_no_tool.forward(joint_angles)
-            self.robot_no_tool.draw()
+            robot_no_tool.forward(joint_angles)
+            robot_no_tool.draw()
 
         with self.subTest('with tool'):
-            self.robot_with_tool.forward(joint_angles)
-            self.robot_with_tool.draw()
+            robot_with_tool.forward(joint_angles)
+            robot_with_tool.draw()
 
         with self.subTest('plot size specified (distorted), remove cylinders and orientation markers'):
-            self.robot_specify_plot_size.forward(joint_angles)
-            self.robot_specify_plot_size.draw(cylinder_relative_size=0, orientation_relative_size=0)
+            robot_specify_plot_size.forward(joint_angles)
+            robot_specify_plot_size.draw(cylinder_relative_size=0, orientation_relative_size=0)
+
+        plt.show()
+
+    def test_draw_with_slider(self):
+        robot = RobotSerial(
+            dh_params=self.dh_params,
+            dh_type="normal",
+            tool=Tool(self.t_4_4),
+            ws_lim=None
+        )
+        numberOfPositions = 100
+        joint_angles = np.array([np.arange(1, 2, 1/numberOfPositions),
+                                  np.arange(-2, 0, 2/numberOfPositions),
+                                  np.arange(2, 1, -1/numberOfPositions),
+                                  np.arange(1, 2, 1/numberOfPositions),
+                                  np.arange(1, 0, -1/numberOfPositions),
+                                  np.arange(1, 3, 2/numberOfPositions)]).T
+        # draw start position
+        position = joint_angles[0]
+        robot.forward(position)
+        robot.draw()
+
+        # create slider with update function
+        axSlider: plt.Axes = plt.axes([0.15, 0.06, 0.75, 0.05])
+        self._slider = Slider(axSlider, "progress", 0.0, 1.0, valinit=0)
+
+        def update(_):
+            position_index = int(self._slider.val * (numberOfPositions - 1))
+            position = joint_angles[position_index]
+            robot.forward(position)
+            robot.draw()
+
+        self._slider.on_changed(update)
 
         plt.show()
 
@@ -69,15 +102,26 @@ class TestRobotSerial(TestCase):
         joint_angles_exp = np.array([1, -2, 2, 1, 1, 1])
 
         with self.subTest('without tool'):
-            end_frame = self.robot_no_tool.forward(joint_angles_exp)
-            joint_angles = self.robot_no_tool.inverse(end_frame)
+            robot_no_tool = RobotSerial(
+                dh_params=self.dh_params,
+                dh_type="normal",
+                ws_lim=None
+            )
+            end_frame = robot_no_tool.forward(joint_angles_exp)
+            joint_angles = robot_no_tool.inverse(end_frame)
             [self.assertAlmostEqual(joint_angle, joint_angle_exp)
              for joint_angle, joint_angle_exp
              in zip(joint_angles, joint_angles_exp)]
 
         with self.subTest('with tool'):
-            end_frame = self.robot_with_tool.forward(joint_angles_exp)
-            joint_angles = self.robot_with_tool.inverse(end_frame)
+            robot_with_tool = RobotSerial(
+                dh_params=self.dh_params,
+                dh_type="normal",
+                tool=Tool(self.t_4_4),
+                ws_lim=None
+            )
+            end_frame = robot_with_tool.forward(joint_angles_exp)
+            joint_angles = robot_with_tool.inverse(end_frame)
             [self.assertAlmostEqual(joint_angle, joint_angle_exp)
              for joint_angle, joint_angle_exp
              in zip(joint_angles, joint_angles_exp)]
